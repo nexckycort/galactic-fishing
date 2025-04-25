5f9e01def21b496c4f20a0251765ca459cb86596
nexckycort <nexckycort@gmail.com>
2025-04-24 23:37:22 -0500

    feat: add service worker support with caching strategies
    - Integrated Workbox's GenerateSW plugin to create a service worker for caching assets and API responses.
    - Configured runtime caching for leaderboard and market API endpoints with specified cache names and expiration settings.
    - Updated the application entry point to register the service worker on load.

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginBabel } from "@rsbuild/plugin-babel";
 import { pluginSolid } from "@rsbuild/plugin-solid";
 import { RsdoctorRspackPlugin } from "@rsdoctor/rspack-plugin";
+import { GenerateSW } from "@aaroon/workbox-rspack-plugin";
 
 export default defineConfig({
 	plugins: [
@@ -10,9 +11,47 @@ export default defineConfig({
 		}),
 		pluginSolid(),
 	],
+	html: {
+		title: "Galactic Fishing",
+	},
 	tools: {
 		rspack: {
-			plugins: import.meta.env.RSDOCTOR && [new RsdoctorRspackPlugin({})],
+			plugins: [
+				new GenerateSW({
+					swDest: "service-worker.js",
+					clientsClaim: true,
+					skipWaiting: true,
+					maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
+					navigateFallback: "/index.html",
+					runtimeCaching: [
+						{
+							urlPattern:
+								/^https:\/\/api-game\.bloque\.app\/game\/leaderboard$/,
+							handler: "NetworkFirst",
+							options: {
+								cacheName: "leaderboard-cache",
+								expiration: {
+									maxEntries: 10,
+									maxAgeSeconds: 60 * 10, // 5 minutos
+								},
+							},
+						},
+						{
+							urlPattern: /^https:\/\/api-game\.bloque\.app\/game\/market$/,
+							handler: "NetworkFirst",
+							options: {
+								cacheName: "market-cache",
+								expiration: {
+									maxEntries: 10,
+									maxAgeSeconds: 60 * 10, // 10 minutos
+								},
+							},
+						},
+					],
+				}),
+				// @ts-expect-error
+				import.meta.env.RSDOCTOR && new RsdoctorRspackPlugin(),
+			].filter(Boolean),
 		},
 	},
 });
--- src/index.tsx
+++ src/index.tsx
@@ -7,3 +7,11 @@ const root = document.getElementById("root");
 if (root) {
 	render(() => <App />, root);
 }
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("/service-worker.js")
+			.catch((err) => console.error("SW registration failed:", err));
+	});
+}
